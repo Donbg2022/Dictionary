bb81c9b57b3e04a6ba19ae891f28a09ff91a146d
Donovan <donovangriffin04@gmail.com>
2022-11-15 10:18:00 -0800

    added loop to display all definitions from the api

---
+++ app.js
@@ -0,0 +1,26 @@
+//create DOM variables
+const input = document.querySelector('#input');
+const search = document.querySelector('#search')
+let word = ''
+let wordUrl = ``
+
+
+search.addEventListener('click', wordSearch)
+// async function used in a click event listener do add definitions to page
+async function wordSearch(){
+  //updates word variable so it can be used by axios get on event click
+  word = input.value
+  //await get request for specidic word user searches
+  const search = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+  //gets the nested definition from api
+  let wordMeaning = search.data[0].meanings
+  //loops through the variety of options from api to display them all
+  for (let i = 0; i < wordMeaning.length; i++) {
+    console.log(wordMeaning[i].definitions[0].definition);
+    
+  }  
+  console.log(search.data)
+  }
+  // [0].meanings[0].definitions
+
+
